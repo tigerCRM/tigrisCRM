4958086c95446bcd53ddc1d3d2532b87d6f8f7b0
남유경 <ygnam0316@gmail.com>
2024-11-12 15:25:37 +0900

    feat : ticket list 호출 로직 수정, T_TICKET_INFO 테이블 create

--- src/main/java/com/tiger/crm/repository/mapper/TicketMapper.java
+++ src/main/java/com/tiger/crm/repository/mapper/TicketMapper.java
@@ -1,12 +1,10 @@
 package com.tiger.crm.repository.mapper;
 
-import com.tiger.crm.repository.dto.TicketDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+import java.util.Map;
 @Mapper
 public interface TicketMapper {
-    List<TicketDto> getTicketList();
-
+    List<Map<String, Object>> getTicketList();
 }
--- src/main/java/com/tiger/crm/service/TicketService.java
+++ src/main/java/com/tiger/crm/service/TicketService.java
@@ -1,18 +1,18 @@
 package com.tiger.crm.service;
 
 import java.util.List;
-import com.tiger.crm.repository.dto.TicketDto;
 import com.tiger.crm.repository.mapper.TicketMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class TicketService {
-
     @Autowired
     private TicketMapper ticketMapper;
 
-    public List<TicketDto> getTicketList() {
+    public List<Map<String, Object>> getTicketList() {
         return ticketMapper.getTicketList();
     }
 }
