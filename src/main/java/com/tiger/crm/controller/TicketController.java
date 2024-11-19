ba65f0ca0ba8dc3fd7789ceaacb91598a626bbbc
남유경 <ygnam0316@gmail.com>
2024-11-19 10:13:58 +0900

    feat : 프로젝트내 불필요한 파일정리

--- src/main/java/com/tiger/crm/controller/TicketController.java
+++ src/main/java/com/tiger/crm/controller/TicketController.java
@@ -8,6 +8,7 @@ import com.tiger.crm.service.ticket.TicketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +24,8 @@ public class TicketController {
 
     private Logger LOGGER = LoggerFactory.getLogger(getClass());
 
+   /* @Value("${status.options}")
+    private String[] statusOptions;*/
     /*
      * 요청관리(티켓관리)
      * 설명 : 요청관리 페이지 초기화면
--- src/main/java/com/tiger/crm/service/org/OrgService.java
+++
@@ -1,7 +0,0 @@
-//package com.example.crm.service.org;
-//
-//
-//public interface OrgService
-//{
-//	Object getOrgTree();
-//}
