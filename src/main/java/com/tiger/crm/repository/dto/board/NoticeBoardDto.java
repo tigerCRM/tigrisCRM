7e39b91ecdcb8482fbc0a8412f0ca18f66e92f44
yesolje <flowerbud94@gmail.com>
2024-12-05 13:34:41 +0900

    docs : mapper.xml 대소문자 정리, sql 문서 대소문자 및 코멘트 작성, dto 주석

--- src/main/java/com/tiger/crm/repository/dto/board/NoticeBoardDto.java
+++ src/main/java/com/tiger/crm/repository/dto/board/NoticeBoardDto.java
@@ -13,24 +13,41 @@ import java.util.List;
 * */
 @Data
 public class NoticeBoardDto {
+    //게시판번호
     private int boardId;
+    //게시판 카테고리
     private String categoryCd;
-    private String companyId; //이거 지워도 될지?
-    private String companyName;//이거 지워도 될지?
+    private String companyId; //이거 지워도 될지 고민중
+    private String companyName;//이거 지워도 될지 고민중
+    //열람제한
     private char openYn;
+    //상단고정여부
     private char topYn;
+    //팝업여부
     private char popupYn;
+    //팝업시작일자
     private String popupStartDt;
+    //팝업종료일자
     private String popupEndDt;
+    //제목
     private String title;
+    //내용
     private String content;
+    //작성자아이디
     private String createId;
+    //작성자명
     private String userName;
+    //작성일
     private Date createDt;
+    //수정자명
     private String updateId;
+    //수정일
     private Date updateDt;
+    //삭제여부
     private char deleteYn;
+    //파일아이디
     private String fileId;
+    //첨부파일(멀티파트)
     private List<MultipartFile> attachFiles;
 
 }
