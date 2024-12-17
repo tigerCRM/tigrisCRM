f325e3933bf8250edb8d4b059735052ab4e4286f
yesolje <flowerbud94@gmail.com>
2024-12-17 14:39:59 +0900

    fix : 시스템관리, 공지사항 DB 에 첨부파일 없어도 FILE_ID 찍히는 현상 수정2

--- src/main/java/com/tiger/crm/controller/NoticeBoardController.java
+++ src/main/java/com/tiger/crm/controller/NoticeBoardController.java
@@ -293,10 +293,12 @@ public class NoticeBoardController {
             }
 
             //신규로 추가된 파일이 있으면 첨부 작업
-            LOGGER.info("새로 들어온 파일의 크기 : " + noticeBoard.getAttachFiles().size());
-            List<UploadFileDto> uploadFiles = fileStoreUtils.storeFiles(noticeBoard.getAttachFiles()); // 경로에 저장
-            String fileId = fileService.insertFile(uploadFiles, noticeBoard.getBoardId(), "공지사항"); //DB 에 저장
-            noticeBoardService.setNoticeBoardFileId(fileId,noticeBoard.getBoardId());//DB에 저장
+            if(!noticeBoard.getAttachFiles().get(0).isEmpty()){
+                LOGGER.info("새로 들어온 파일의 크기 : " + noticeBoard.getAttachFiles().size());
+                List<UploadFileDto> uploadFiles = fileStoreUtils.storeFiles(noticeBoard.getAttachFiles()); // 경로에 저장
+                String fileId = fileService.insertFile(uploadFiles, noticeBoard.getBoardId(), "공지사항"); //DB 에 저장
+                noticeBoardService.setNoticeBoardFileId(fileId,noticeBoard.getBoardId());//DB에 저장
+            }
 
         }catch (Exception e){
             LOGGER.info("게시물 수정 오류 발생 : " + e);
