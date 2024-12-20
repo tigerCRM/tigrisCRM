7f2163ca770db55533d3a9904a11ed993a0f0697
남유경 <ygnam0316@gmail.com>
2024-12-20 11:48:35 +0900

    feat : 좌측메뉴 수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -81,6 +81,40 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+$(document).ready(function(){
+    $('#adminmenu').click(function(e){
+        e.preventDefault(); // 링크 기본 동작 방지
+
+        // 클릭한 메뉴의 하위 메뉴를 찾고 토글 (보이기/숨기기)
+        var subMenu = $(this).next('.tree-list--sub-2');
+        subMenu.slideToggle(); // 슬라이딩 효과로 토글
+
+        // 아이콘 방향 변경 (선택 사항)
+        var arrowBtn = $(this).find('.tree-arrow-btn');
+        arrowBtn.toggleClass('tree--open'); // 아이콘 상태 변경
+    });
+    /*// 메뉴 클릭 시 오른쪽 컨텐츠만 업데이트
+    $('.menu-item').click(function(e) {
+        e.preventDefault(); // 기본 링크 동작 방지
+
+        var url = $(this).data('url'); // 클릭한 메뉴의 URL 가져오기
+
+        // AJAX로 오른쪽 컨텐츠 로드
+        $.ajax({
+            url: url,
+            method: 'GET',
+            success: function(response) {
+                // 오른쪽 영역에 새로운 콘텐츠를 로드
+                $('.main-area').html(response);
+            },
+            error: function() {
+                alert('콘텐츠를 불러오는 데 오류가 발생했습니다.');
+            }
+        });
+    });*/
+});
+
+
 //2. 함수 저장소  //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var common = {
 
