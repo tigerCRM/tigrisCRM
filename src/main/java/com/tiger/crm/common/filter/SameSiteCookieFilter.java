85dd4e4f70e6cbfb089450772cdac85a9ae0d329
yesolje <flowerbud94@gmail.com>
2024-12-25 10:13:12 +0900

    feat : samsite 설정 위해 cookieFilter 추가

---
+++ src/main/java/com/tiger/crm/common/filter/SameSiteCookieFilter.java
@@ -0,0 +1,44 @@
+package com.tiger.crm.common.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class SameSiteCookieFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        // 모든 쿠키를 가져옴
+        Cookie[] cookies = httpServletRequest.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                // JSESSIONID 쿠키를 찾음
+                if ("JSESSIONID".equals(cookie.getName())) {
+                    // SameSite 속성과 기타 속성을 설정
+                    Cookie newCookie = new Cookie(cookie.getName(), cookie.getValue());
+                    newCookie.setPath("/");
+                    newCookie.setHttpOnly(true);
+                    newCookie.setSecure(true); // HTTPS 환경일 경우
+                    newCookie.setMaxAge(cookie.getMaxAge()); // 기존 쿠키 만료시간 유지
+                    newCookie.setDomain(httpServletRequest.getServerName());
+                    // SameSite 속성을 추가 (Lax 또는 Strict)
+                    httpServletResponse.addHeader("Set-Cookie",
+                            newCookie.getName() + "=" + newCookie.getValue()
+                                    + "; Path=" + newCookie.getPath()
+                                    + "; HttpOnly; Secure; SameSite=Lax");
+                }
+            }
+        }
+
+        // 필터 체인 진행
+        chain.doFilter(request, response);
+    }
+}
