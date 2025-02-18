cb4eeff77b61cb2d77f9de88ac0400f45b4575e5
남유경 <ygnam0316@gmail.com>
2025-02-18 16:10:16 +0900

    feat : 만족도 및 통계 수정 시작

--- src/main/java/com/tiger/crm/controller/AnalyticsController.java
+++ src/main/java/com/tiger/crm/controller/AnalyticsController.java
@@ -8,12 +8,14 @@ import com.tiger.crm.repository.dto.page.PagingResponse;
 import com.tiger.crm.repository.dto.user.UserLoginDto;
 import com.tiger.crm.service.analytics.AnalyticsService;
 import com.tiger.crm.service.common.CommonService;
+import com.tiger.crm.service.ticket.TicketService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,12 +38,13 @@ public class AnalyticsController {
     private final CommonService commonService;
     private final AnalyticsService analyticsService;
     private Logger LOGGER = LoggerFactory.getLogger(getClass());
-
+    @Autowired
+    private TicketService ticketService;
     /*
     * 주간처리현황 페이지 보기
     * */
     @GetMapping("/analytics_week")
-    public String getAnalyticsWeek(HttpServletRequest request, HttpServletResponse response, Model model)
+    public String getAnalyticsWeek(HttpServletRequest request, HttpServletResponse response, Model model,PagingRequest pagingRequest)
     {
         HttpSession session = request.getSession(false);
         UserLoginDto loginUser = (UserLoginDto)session.getAttribute("loginUser");
@@ -60,11 +63,15 @@ public class AnalyticsController {
         List<AnalyticsWeekDto> weekReceiptList = analyticsService.getAnalyticsWeek(startDate);
         model.addAttribute("weekReceiptList", weekReceiptList);
 
+        // 요청 조회
+        PagingResponse<Map<String, Object>> pageResponse = ticketService.getTicketList(pagingRequest);
+        model.addAttribute("ticketList", pageResponse);
+
         return "analyticsWeek";
     }
 
     @PostMapping("/analytics_week")
-    public String getAnalyticsWeekSearchWeek(HttpServletRequest request, HttpServletResponse response, Model model) {
+    public String getAnalyticsWeekSearchWeek(HttpServletRequest request, HttpServletResponse response, Model model,PagingRequest pagingRequest) {
 
         String startDate = request.getParameter("startDate");
 
@@ -79,7 +86,18 @@ public class AnalyticsController {
         //접수 리스트
         List<AnalyticsWeekDto> weekReceiptList = analyticsService.getAnalyticsWeek(startDate);
         model.addAttribute("weekReceiptList", weekReceiptList);
-
+        // 요청 조회
+        PagingResponse<Map<String, Object>> pageResponse = ticketService.getTicketList(pagingRequest);
+        model.addAttribute("ticketList", pageResponse);
         return "analyticsWeek :: analyticsWeekFragment";
+    }
+
+    @PostMapping("/analytics_week_detail")
+    public String getAnalyticsWeekSearchWeekDetail(HttpServletRequest request, HttpServletResponse response, Model model,PagingRequest pagingRequest) {
+
+        // 요청 조회
+        PagingResponse<Map<String, Object>> pageResponse = ticketService.getTicketList(pagingRequest);
+        model.addAttribute("ticketList", pageResponse);
+        return "analyticsWeek :: analyticsWeekDetail";
     }
 }
